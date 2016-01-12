ea2b343b16be01dd10962c529c09e78132f2001d
daanpeters <d.p.c.h.peters@student.tue.nl>
2016-01-12 23:50:47 +0100

    update

--- leshan/leshan-standalone/src/main/java/org/eclipse/leshan/standalone/servlet/ClientServlet.java
+++ leshan/leshan-standalone/src/main/java/org/eclipse/leshan/standalone/servlet/ClientServlet.java
@@ -99,7 +99,7 @@ public class ClientServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    	resp.addHeader("Access-Control-Allow-Origin", "*");
         // all registered clients
         if (req.getPathInfo() == null) {
             Collection<Client> clients = server.getClientRegistry().allClients();
